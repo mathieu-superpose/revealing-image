f50e2ab6b50c125f51a2369bfd892964b564773a
Mathieu Joly <superpose@minimac.local>
2025-04-17 15:54:34 +0200

    add several images

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,41 @@ function App() {
   return (
     <div
       style={{
-        height: "100vh",
+        minHeight: "100vh",
         width: "100%",
+        paddingBottom: "100px",
       }}
     >
-      <h1 style={{ padding: "40px" }}>Revealing Image</h1>
+      <h1 style={{ padding: "40px", marginBottom: "100px" }}>
+        Revealing Image
+      </h1>
 
-      <div
+      <ul
         style={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          listStyle: "none",
+          gap: "200px",
         }}
       >
-        <RevealingImage height={600} width={800} path="img/bmx.jpg" />
-      </div>
+        <li style={{ alignSelf: "start" }}>
+          <RevealingImage height={600} width={800} path="img/bmx.jpg" />
+        </li>
+        <li style={{ alignSelf: "end" }}>
+          <RevealingImage height={600} width={800} path="img/love.jpg" />
+        </li>
+
+        <li style={{ alignSelf: "start" }}>
+          <RevealingImage height={600} width={800} path="img/river.jpg" />
+        </li>
+        <li style={{ alignSelf: "end" }}>
+          <RevealingImage height={600} width={800} path="img/running.jpg" />
+        </li>
+        <li style={{ alignSelf: "start" }}>
+          <RevealingImage height={600} width={800} path="img/subway.jpg" />
+        </li>
+      </ul>
     </div>
   )
 }
